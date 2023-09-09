6efd82bb2235cfa3a853206f2a2d1e3ff4a68b0b
laboratorioesan <laboratorioesan@esan.edu.pe>
2023-09-09 09:24:55 -0500

    primer lab

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Firstappsem2MG(020923)"
+include(":app")
