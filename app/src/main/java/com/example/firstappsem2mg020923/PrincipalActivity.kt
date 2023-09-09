17265efe1cb612ab5773fb718416ca5e0d7912e0
MartínRGC <martingarcia08.00@gmail.com>
2023-09-09 09:53:05 -0500

    Agregué un holaaaaaa, coninua

--- app/src/main/java/com/example/firstappsem2mg020923/PrincipalActivity.kt
+++ app/src/main/java/com/example/firstappsem2mg020923/PrincipalActivity.kt
@@ -1,6 +1,9 @@
 package com.example.firstappsem2mg020923
 
+import android.os.Bundle
+import android.util.Log
+
 class PrincipalActivity {
-    //codear
-    //hi world
+    //Holaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
 }
